e11891f01a5f0d71ff1aa214fd47cd2108e16a2c
Ngoc Le <ngocle@MacBook-Pro-de-Ngoc.local>
2020-12-06 20:53:45 +0100

    FirstController added

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -1,10 +1,39 @@
 <?php
+
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class FirstController extends AbstractController
 
 {
 
+    /**
+     * @Route('/', name='index')
+     */
+    public function index()
+    {
+        return $this->render('base.html.twig');
+    }
+
+    /**
+     * @Route('/products', name='products')
+     */
+    function showProducts(ProductRepository $productRepository)
+    {
+        $products = $productRepository->findAll();
+        return $this->render('test.html.twig', ['products' => $products]);
+    }
+
+    /**
+     * @Route("/category", name="category")
+     */
+    public function category(CategoryRepository $categoryRepository)
+    {
+        $categories = $categoryRepository->findAll();
+        return $this->render('category.html.twig', ['categories' => $categories]);
+    }
 }
