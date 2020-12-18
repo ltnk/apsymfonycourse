44347a8d2b927b2f9053b3f6689edb3812ca7ffa
Ngoc Le <ngocle@MacBook-Pro-de-Ngoc.local>
2020-12-18 17:08:25 +0100

    Crud user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,29 +2,115 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpParser\Node\Stmt\TryCatch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+/**
+ * @Route("/profile/user")
+ */
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/", name="user_index", methods={"GET"})
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'users' => $userRepository->findAll(),
         ]);
     }
 
     /**
-     * @Route("/profile/users", name="profileUsers")
+     * @Route("/add", name="addUser", methods={"GET","POST"})
      */
-    public function usersList(UserRepository $userRepository)
+    public function addUser(UserPasswordEncoderInterface $passwordEncoder, UserRepository $userRepository, Request $request, EntityManagerInterface $em): Response
     {
-        $users = $userRepository->findAll();
-        return $this->render('user/index.html.twig', ['users' => $users]);
+
+        $user = new User;
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        try {
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                $user = $form->getData();
+                $roles = $form->get('roles')->getData();
+    
+                $user->setRoles([0 => $roles]);
+    
+                $plainPassword = $form['password']->getData();
+    
+                if(trim($plainPassword) != ''){
+                    $password = $passwordEncoder->encodePassword($user, $plainPassword);
+                    $user->setPassword($password);
+    
+                }
+                $em->persist($user);
+                $em->flush();
+    
+                // Go directly on the page users after removing the users
+                $users = $userRepository->findAll();
+                return $this->render('user/index.html.twig', ['users' => $users]);
+            }
+        } catch (\Throwable $th) {
+            $this->addFlash('notice', 'Votre mot de passe semble incorrect !');
+        }
+
+        return $this->render('user/new.html.twig', ['form' => $form->createView()]);
     }
+
+    /**
+     * @Route("/{id}", name="showUser", methods={"GET"})
+     */
+    public function showUser(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="editUser", methods={"GET","POST"})
+     */
+    public function editUser(Request $request, User $user): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('user_index');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="deleteUser", methods={"DELETE"})
+     */
+    public function deleteUser(Request $request, User $user): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('user_index');
+    }
+
 }
+
---
+++ src/Form/UserType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\Length;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'User' => 'ROLE_USER',
+                    'Employé' => 'ROLE_STAFF',
+                    'Admin' => 'ROLE_ADMIN',
+                ],
+                'help' => 'Sélectionnez un rôle',
+                'multiple' => false,
+                'mapped' => false,
+            ])
+            ->add('password', PasswordType::class, [
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [new Length([
+                    'min' => 3,
+                ])],
+            ])
+            ->add('save', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
